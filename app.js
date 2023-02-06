8f43cc56c0d8b5ea7f65ea4e322a1d12a4629c00
DaKss <112929982+DaKosss@users.noreply.github.com>
2023-02-06 17:55:38 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+// Task1
+// a = 0;
+// setInterval(function()
+// {
+//     a++;
+//     console.log('Hello world ' + a);
+//     if (a >= 150) stop;
+// });
+
+// Task2
+
+// n = 2
+
+// setInterval(function()
+// {
+//     console.log(n);
+//     n += 2;
+//     if (n == 22) stop;
+// });
+    
+
+
+// Task3
+// num = -2;
+
+// function task3 (num) {
+
+//     if(num > 0) console.log("Hello");
+
+//     if(num < 0) console.log("olleH");
+// }
+
+// task3(num);
+
+
+// Task4
+// const fs = require('fs');
+// let text = fs.readFileSync('readme.txt', 'utf8')
+
+// num1 = 12;
+// num2 = 9;
+
+// function task4 (num1, num2) {
+//     fs.writeFileSync('readme.txt', text + num1 * num2);
+// }
+
+// task4(num1, num2);
